4203782f311f82aad866594336ff4948cbef2ac4
Holden Prine <holdenprine404@gmail.com>
2025-03-11 17:08:27 -0700

    Initial Commit

---
+++ TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by Holden Prine on 3/11/25.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

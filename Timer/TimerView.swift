036d43d1e9e95813af7881f47efb62892aedbb7b
Holden Prine <holdenprine404@gmail.com>
2025-03-12 10:52:23 -0700

    Basic Timer functionality achieved, UX and styling still needed, no issues atm.

--- Timer/TimerView.swift
+++ Timer/TimerView.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 struct TimerView: View {
     
-    @State private var timeRemaining = 300
+    @State private var timeRemaining = 40
     
     var body: some View {
-        Text("\(timeRemaining)")
+        Text(timeString(from: timeRemaining))
             .font(.largeTitle)
             .padding()
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                    if(timeRemaining > 0) {
+                        timeRemaining -= 1
+                    } else {
+                        timer.invalidate()
+                    }
+                }
+            }
+    }
+    
+    private func timeString(from seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
